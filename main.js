679a1504a5fb3886e067a4b9e8d6e8d671769853
João Macedo <jpdm060509@gmail.com>
2025-04-08 23:22:53 -0300

    Criação da função para calcular todos os resultados

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ let bill = 0
  let numberOfPeople = 0
  let buttonSelected = null
  
-  function receiveBillValue(){
-     bill = document.querySelector("#bill").valueAsNumber
-  }
- 
-  function receiveNumberOfPeopleValue(){
-     numberOfPeople = document.querySelector("#people").valueAsNumber
-  }
- 
-  function receiveTipPercentageValue(value){
+ function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+    calculateResults()
+ }
+
+ function receiveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculateResults()
+ }
+
+ function receiveTipPercentageValue(value){
     tipPercentage = value / 100
 
     document.querySelector("#custom-tip").value = ""
@@ -21,18 +23,32 @@ let bill = 0
     }
 
     buttonSelected = document.querySelector(`#button-${value}`)
-    buttonSelected.classList.add("button-selected")
- }
-
- function receiveCustomTipPercentageValue() {
-    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
-
-    removeClassButtonSelected()
- }
-
- function removeClassButtonSelected() {
-    if (buttonSelected !== null) {
-        buttonSelected.classList.remove("button-selected")
-        buttonSelected = null
-    }
- }
+     buttonSelected.classList.add("button-selected")
+     calculateResults()
+  }
+ 
+  function receiveCustomTipPercentageValue() {
+     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+ 
+     removeClassButtonSelected()
+     calculateResults()
+  }
+ 
+  function removeClassButtonSelected() {
+     if (buttonSelected !== null) {
+         buttonSelected.classList.remove("button-selected")
+         buttonSelected = null
+     }
+  }
+ 
+  function calculateResults() {
+     if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !==0) {
+         let strongAmount = document.querySelector(".amount strong")
+         let tipAmountPerson = bill * tipPercentage / numberOfPeople
+         strongAmount.innerText = `$${tipAmountPerson.toFixed(2)}`
+ 
+         let strongTotal = document.querySelector(".total strong")
+         let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+         strongTotal.innerText = `$${totalPerson.toFixed(2)}`
+     }
+  }
